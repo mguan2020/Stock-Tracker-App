432099290373108c58a83af3ab7f02ff786c91e6
mguan2020 <mguan988@ucla.edu>
2023-09-21 16:14:19 -0700

    Added stock lookup form and search bar. It displays stock search results in the browser.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,8 @@ class Stock < ApplicationRecord
         client = IEX::Api::Client.new(publishable_token: 'pk_3747321d71134bb988e264418a0ed62f',
     endpoint: 'https://cloud.iexapis.com/v1')
 
-        client.price(ticker_symbol)
+        #client.price(ticker_symbol)
+
+        new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
