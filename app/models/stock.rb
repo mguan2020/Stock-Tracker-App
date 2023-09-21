acd211cf4a00cba1a9c77d689de7018ba2420490
mguan2020 <mguan988@ucla.edu>
2023-09-21 16:23:28 -0700

    Handle invalid user input in search stock form

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,6 +6,11 @@ class Stock < ApplicationRecord
 
         #client.price(ticker_symbol)
 
+        begin
         new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+        rescue => exception
+         return nil
+
+        end
     end
 end
